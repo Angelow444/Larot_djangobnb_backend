9a062d23387d1993eb3ddc2b3a8a078ae676e9af
Angelow143 <Angelowlarot@gmail.com>
2025-11-13 06:39:04 +0800

    3rd Video_Create app for properties -Angelow B. Larot

--- djangobnb_backend/entrypoint.sh
+++ djangobnb_backend/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
+if [ "$DATABASE" = "postgres" ] 
 then
-    echo "Waiting for postgres..."
+    echo "Check if database is running..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
+        sleep 0.1
     done
 
-    echo "PostgreSQL started"
+    echo "The database is up and running :-D"
 fi
 
-python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
 
 exec "$@"
